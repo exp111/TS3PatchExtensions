e9a895040de5c7e3d8c40f256d7a8503d241af07
Exp <theexp111@gmail.com>
2018-08-12 20:54:04 +0200

    fuck qt

--- customBadges/GeneratedFiles/ReleaseAll/moc_QtConfig.cpp
+++ customBadges/GeneratedFiles/ReleaseAll/moc_QtConfig.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_QtConfig_t {
-    QByteArrayData data[12];
-    char stringdata0[133];
+    QByteArrayData data[11];
+    char stringdata0[121];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,21 +32,20 @@ struct qt_meta_stringdata_QtConfig_t {
 static const qt_meta_stringdata_QtConfig_t qt_meta_stringdata_QtConfig = {
     {
 QT_MOC_LITERAL(0, 0, 8), // "QtConfig"
-QT_MOC_LITERAL(1, 9, 11), // "updateBoxes"
-QT_MOC_LITERAL(2, 21, 0), // ""
-QT_MOC_LITERAL(3, 22, 12), // "saveToConfig"
-QT_MOC_LITERAL(4, 35, 8), // "addBadge"
-QT_MOC_LITERAL(5, 44, 16), // "QTreeWidgetItem*"
-QT_MOC_LITERAL(6, 61, 12), // "deleteBadge1"
-QT_MOC_LITERAL(7, 74, 12), // "deleteBadge2"
-QT_MOC_LITERAL(8, 87, 12), // "deleteBadge3"
-QT_MOC_LITERAL(9, 100, 9), // "swapBadge"
-QT_MOC_LITERAL(10, 110, 5), // "label"
-QT_MOC_LITERAL(11, 116, 16) // "openManualBadges"
+QT_MOC_LITERAL(1, 9, 12), // "saveToConfig"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 8), // "addBadge"
+QT_MOC_LITERAL(4, 32, 16), // "QTreeWidgetItem*"
+QT_MOC_LITERAL(5, 49, 12), // "deleteBadge1"
+QT_MOC_LITERAL(6, 62, 12), // "deleteBadge2"
+QT_MOC_LITERAL(7, 75, 12), // "deleteBadge3"
+QT_MOC_LITERAL(8, 88, 9), // "swapBadge"
+QT_MOC_LITERAL(9, 98, 5), // "label"
+QT_MOC_LITERAL(10, 104, 16) // "openManualBadges"
 
     },
-    "QtConfig\0updateBoxes\0\0saveToConfig\0"
-    "addBadge\0QTreeWidgetItem*\0deleteBadge1\0"
+    "QtConfig\0saveToConfig\0\0addBadge\0"
+    "QTreeWidgetItem*\0deleteBadge1\0"
     "deleteBadge2\0deleteBadge3\0swapBadge\0"
     "label\0openManualBadges"
 };
@@ -58,7 +57,7 @@ static const uint qt_meta_data_QtConfig[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -66,23 +65,21 @@ static const uint qt_meta_data_QtConfig[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x0a /* Public */,
-       3,    0,   55,    2, 0x0a /* Public */,
-       4,    1,   56,    2, 0x0a /* Public */,
-       6,    0,   59,    2, 0x0a /* Public */,
-       7,    0,   60,    2, 0x0a /* Public */,
-       8,    0,   61,    2, 0x0a /* Public */,
-       9,    2,   62,    2, 0x0a /* Public */,
-      11,    0,   67,    2, 0x0a /* Public */,
+       1,    0,   49,    2, 0x0a /* Public */,
+       3,    1,   50,    2, 0x0a /* Public */,
+       5,    0,   53,    2, 0x0a /* Public */,
+       6,    0,   54,    2, 0x0a /* Public */,
+       7,    0,   55,    2, 0x0a /* Public */,
+       8,    2,   56,    2, 0x0a /* Public */,
+      10,    0,   61,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 5,    2,
-    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    2,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString, 0x80000000 | 5,   10,    2,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 4,    9,    2,
     QMetaType::Void,
 
        0        // eod
@@ -94,14 +91,13 @@ void QtConfig::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         QtConfig *_t = static_cast<QtConfig *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->updateBoxes(); break;
-        case 1: _t->saveToConfig(); break;
-        case 2: _t->addBadge((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1]))); break;
-        case 3: _t->deleteBadge1(); break;
-        case 4: _t->deleteBadge2(); break;
-        case 5: _t->deleteBadge3(); break;
-        case 6: _t->swapBadge((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QTreeWidgetItem*(*)>(_a[2]))); break;
-        case 7: _t->openManualBadges(); break;
+        case 0: _t->saveToConfig(); break;
+        case 1: _t->addBadge((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1]))); break;
+        case 2: _t->deleteBadge1(); break;
+        case 3: _t->deleteBadge2(); break;
+        case 4: _t->deleteBadge3(); break;
+        case 5: _t->swapBadge((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QTreeWidgetItem*(*)>(_a[2]))); break;
+        case 6: _t->openManualBadges(); break;
         default: ;
         }
     }
@@ -132,13 +128,13 @@ int QtConfig::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 7;
     }
     return _id;
 }
