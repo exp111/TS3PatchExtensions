4e0a44c1a53626e20ef28c2c0362bcce540973dc
Exp <theexp111@gmail.com>
2018-08-24 00:20:13 +0200

    fuck

--- connectionInfoSpoofer/QtConfig.cpp
+++ connectionInfoSpoofer/QtConfig.cpp
@@ -19,12 +19,12 @@ QtConfig::QtConfig(QWidget *parent)
 	for (int i = 0; i < setConnectionInfoSize; i++)
 	{
 		ui.tableWidget->insertRow(i);
-		QTableWidgetItem* item = new QTableWidgetItem(QString(get<0>(config->setConnectionInfo[i]).c_str()));
+		QTableWidgetItem* item = new QTableWidgetItem(QString::fromStdString(config->setConnectionInfo[i].name));
 		ui.tableWidget->setItem(i, 0, item);
-		QTableWidgetItem* item2 = new QTableWidgetItem(QString(get<1>(config->setConnectionInfo[i]).c_str()));
+		QTableWidgetItem* item2 = new QTableWidgetItem(QString::fromStdString(config->setConnectionInfo[i].value));
 		ui.tableWidget->setItem(i, 1, item2);
 		QTableWidgetItem* item3 = new QTableWidgetItem();
-		item3->setCheckState(get<2>(config->setConnectionInfo[i]) ? Qt::Checked : Qt::Unchecked);
+		item3->setCheckState(config->setConnectionInfo[i].useOriginal ? Qt::Checked : Qt::Unchecked);
 		ui.tableWidget->setItem(i, 2, item3);
 	}
 
@@ -33,12 +33,12 @@ QtConfig::QtConfig(QWidget *parent)
 	{
 		int j = (int)setConnectionInfoSize + i;
 		ui.tableWidget->insertRow(j);
-		QTableWidgetItem* item = new QTableWidgetItem(QString(get<0>(config->connectionInfoAutoUpdate[i]).c_str()));
-		ui.tableWidget->setItem(j, 0, item);
-		QTableWidgetItem* item2 = new QTableWidgetItem(QString(get<1>(config->connectionInfoAutoUpdate[i]).c_str()));
+		QTableWidgetItem* item = new QTableWidgetItem(QString::fromStdString(config->setConnectionInfo[i].name));
+		ui.tableWidget->setItem(i, 0, item);
+		QTableWidgetItem* item2 = new QTableWidgetItem(QString::fromStdString(config->setConnectionInfo[i].value));
 		ui.tableWidget->setItem(j, 1, item2);
 		QTableWidgetItem* item3 = new QTableWidgetItem();
-		item3->setCheckState(get<2>(config->connectionInfoAutoUpdate[i]) ? Qt::Checked : Qt::Unchecked);
+		item3->setCheckState(config->connectionInfoAutoUpdate[i].useOriginal ? Qt::Checked : Qt::Unchecked);
 		ui.tableWidget->setItem(j, 2, item3);
 	}
 
@@ -59,16 +59,16 @@ void QtConfig::saveToConfig()
 	size_t setConnectionInfoSize = config->setConnectionInfo.size();
 	for (int i = 0; i < setConnectionInfoSize; i++)
 	{
-		get<1>(config->setConnectionInfo[i]) = ui.tableWidget->item(i, 1)->text().toStdString();
-		get<2>(config->setConnectionInfo[i]) = ui.tableWidget->item(i, 2)->checkState() == Qt::Checked ? true : false;
+		config->setConnectionInfo[i].value = ui.tableWidget->item(i, 1)->text().toStdString();
+		config->setConnectionInfo[i].useOriginal = ui.tableWidget->item(i, 2)->checkState() == Qt::Checked ? true : false;
 	}
 
 	size_t connectionInfoAutoUpdateSize = config->connectionInfoAutoUpdate.size();
 	for (int i = 0; i < connectionInfoAutoUpdateSize; i++)
 	{
 		int j = (int)setConnectionInfoSize + i;
-		get<1>(config->connectionInfoAutoUpdate[i]) = ui.tableWidget->item(j, 1)->text().toStdString();
-		get<2>(config->connectionInfoAutoUpdate[i]) = ui.tableWidget->item(j, 2)->checkState() == Qt::Checked ? true : false;
+		config->connectionInfoAutoUpdate[i].value = ui.tableWidget->item(j, 1)->text().toStdString();
+		config->connectionInfoAutoUpdate[i].useOriginal = ui.tableWidget->item(j, 2)->checkState() == Qt::Checked ? true : false;
 	}
 
 	config->writeConfig();
